d0146fe93cddec83545a33eb7a7a9fb17386393b
JuanPabloBallesterosMaciasUis <juan2224649@correo.uis.edu.co>
2023-04-28 18:39:29 -0500

    Se realiza pequeños cambios

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -1,18 +1,16 @@
 package controlador;
 
 import modelo.SistemaDeTareas;
+import modelo.Tarea;
 import vista.VentanaPrincipal;
 
 public class Controlador {
-    // Definimos los atributos de la clase
     private SistemaDeTareas sistemaDeTareas;
     private VentanaPrincipal vista;
-    
-    // Constructor de la clase
+
     public Controlador(SistemaDeTareas sistemaDeTareas, VentanaPrincipal vista) {
-    // Asignamos los parámetros recibidos a los atributos de la clase
-    this.sistemaDeTareas = sistemaDeTareas;
-    this.vista = vista;
+        this.sistemaDeTareas = sistemaDeTareas;
+        this.vista = vista;
     }
 
     // Métodos para interactuar entre el sistemaDeTareas y la vista
